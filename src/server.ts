9316caa709423d73abcc7eb0edd97e893efb1a69
shagar619 <ashagar619@gmail.com>
2025-10-13 22:58:59 +0600

    implement environment variables

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
@@ -11,12 +12,12 @@ const startServer = async () => {
 
      try {
 
-          await mongoose.connect("mongodb+srv://ToDo_App:ToDo_App@cluster0.hj90b.mongodb.net/PH-tour-management?retryWrites=true&w=majority&appName=Cluster0")
+          await mongoose.connect(envVars.DB_URL)
 
           console.log("Connected to DB!!");
 
-          server =  app.listen(5000, () => {
-               console.log(`Server is listening to port 5000!`);
+          server =  app.listen(envVars.PORT, () => {
+               console.log(`Server is listening to port ${envVars.PORT}!`);
           })
      } catch(error) {
           console.log(error);
