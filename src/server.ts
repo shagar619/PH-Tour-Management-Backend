8df9cfc2e0273b12bd5076475eaa56979b5d3a97
shagar619 <ashagar619@gmail.com>
2025-10-13 19:34:08 +0600

    implement error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { Server } from "http";
-import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import app from "./app";
 
 let server: Server;
 
-const app = express();
+
 
 const startServer = async () => {
 
@@ -24,11 +24,75 @@ const startServer = async () => {
 
 startServer();
 
-app.get("/", (req: Request, res: Response) => {
-     res.status(200).json({
-          message: "Welcome to Tour Management System Backend!"
-     });
+
+process.on("SIGTERM", () => {
+     console.log("SIGTERM signal received... Server shutting down...");
+
+     if (server) {
+          server.close(() => {
+               process.exit(1)
+          });
+     }
+     process.exit(1);
+});
+
+
+process.on("SIGINT", () => {
+     console.log("SIGINT signal received... Server shutting down...");
+
+     if (server) {
+          server.close(() => {
+               process.exit(1)
+          });
+     }
+     process.exit(1);
+});
+
+
+process.on("unhandledRejection", (err) => {
+     console.log(`Unhandled Rejection detected... Server shutting down... ${err}`);
+
+     if(server) {
+          server.close(() => {
+               process.exit(1)
+          });
+     }
+     process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+     console.log(`Uncaught Exception detected... Server shutting down... ${err}`);
+
+     if(server) {
+          server.close(() => {
+               process.exit(1)
+          });
+     }
+     process.exit(1);
 });
 
 
 
+// Un handler rejection error
+// Promise.reject(new Error("I forgot to catch this promise"));
+
+// Uncaught Exception Error
+// throw new Error("I forgot to handle this local error");
+
+
+
+
+
+
+// Un handler rejection error
+// Promise.reject(new Error("I forgot to catch this promise"))
+
+// Uncaught Exception Error
+// throw new Error("I forgot to handle this local error")
+
+
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ */
