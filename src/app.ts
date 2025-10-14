db680b203ef0b8bf5785b194148999703784c5b0
shagar619 <ashagar619@gmail.com>
2025-10-14 22:53:01 +0600

    added api/v1 routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
+import { router } from "./app/routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
      res.status(200).json({
