9eca61e54ae7a2ce72eac3d70248212b8fed96c7
shagar619 <ashagar619@gmail.com>
2025-10-15 23:00:55 +0600

    modify createUser

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,26 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import { UserServices } from "./user.service";
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
+const createUser =catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-     try {
+     const user = await UserServices.createUser(req.body);
 
-          const user = await UserServices.createUser(req.body);
+     sendResponse(res, {
+          success: true,
+          statusCode: httpStatus.CREATED,
+          message: "User Created Successfully",
+          data: user
+     });
 
-          res.status(httpStatus.CREATED).json({
-               message: `User Created Successfully!`,
-               user
-          })
-     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-     } catch(err: any) {
-          // eslint-disable-next-line no-console
-          console.log(err);
-          next(err);
-     }
-}
+
+
+
+
+
+
+
+
+     // try {
+
+     //      const user = await UserServices.createUser(req.body);
+
+     //      res.status(httpStatus.CREATED).json({
+     //           message: `User Created Successfully!`,
+     //           user
+     //      })
+     // } catch(err: any) {
+     //      console.log(err);
+     //      next(err);
+     // }
+
+
+
+
+})
+
+
+
+
+
 
 
 
