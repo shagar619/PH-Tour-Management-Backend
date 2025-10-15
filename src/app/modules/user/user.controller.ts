52dd2d09be81a1b39adef62dabef3e5c5bec18cb
shagar619 <ashagar619@gmail.com>
2025-10-15 23:37:00 +0600

    modify get and created response

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -47,12 +47,26 @@ const createUser =catchAsync(async (req: Request, res: Response, next: NextFunct
 
 
 
+const getAllUsers = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
+
+     const result = await UserServices.getAllUsers();
+
+     sendResponse(res, {
+          success: true,
+          statusCode: httpStatus.CREATED,
+          message: "All Users Retrieved Successfully",
+          data: result.data,
+          meta: result.meta
+     })
+})
+
 
 
 
 
 export const UserControllers = {
      createUser,
+     getAllUsers
 }
 
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -7,6 +7,6 @@ import { UserControllers } from "./user.controller";
 const router = Router();
 
 router.post("/register", UserControllers.createUser);
-// router.get("/all-users", UserControllers.getAllUsers);
+router.get("/all-users", UserControllers.getAllUsers);
 
 export const UserRoutes = router;
