07c905cfed3121e2949eba2ae5f9be0af9b7e283
shagar619 <ashagar619@gmail.com>
2025-10-15 22:47:07 +0600

    added catch async higher order function

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -14,7 +14,22 @@ const createUser = async (payload: Partial<IUser>) => {
 }
 
 
+const getAllUsers = async () => {
+
+     const users = await User.find({});
+     const totalUsers = await User.countDocuments();
+
+     return {
+          data: users,
+          meta : {
+               total: totalUsers
+          }
+     }
+};
+
+
 
 export const UserServices = {
      createUser,
+     getAllUsers
 }
