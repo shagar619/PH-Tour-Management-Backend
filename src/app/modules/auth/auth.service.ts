a1c4693e65dbec4017e3c4f7a7daad1cadcef74a
shagar619 <ashagar619@gmail.com>
2025-10-17 22:57:33 +0600

    added jwt token setup

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -27,7 +28,12 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
           role: isUserExist.role
      }
 
-     return jwtPayload;
+     const accessToken = jwt.sign(jwtPayload, "secretKey", {
+          expiresIn: "1d"
+     });
+
+
+     return { accessToken, ...jwtPayload };
 
 };
 
