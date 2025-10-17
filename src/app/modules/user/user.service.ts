687d9c9b39521b2063d8a6837539f0fd25529e7e
shagar619 <ashagar619@gmail.com>
2025-10-17 20:55:10 +0600

    modify creat user with email verification

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,13 +1,29 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
 
 
 const createUser = async (payload: Partial<IUser>) => {
 
-     const { name, email } = payload;
+     const { email, password, ...rest } = payload;
+
+     const isUserExist = await User.findOne({ email });
+
+     if (isUserExist) {
+          throw new AppError(httpStatus.CONFLICT, "User already exists!");
+     }
+
+     const authProvider: IAuthProvider = {
+          provider: "credentials",
+          providerId: email as string
+     }
+
      const user = await User.create({
-          name,
-          email
+          email,
+          password,
+          auths: [authProvider],
+          ...rest
      });
 
      return user
