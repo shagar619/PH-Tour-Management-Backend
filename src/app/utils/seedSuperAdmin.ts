a99fa79d0b26cb59c98066b4540f86b05fccb307
shagar619 <ashagar619@gmail.com>
2025-10-18 22:48:43 +0600

    modify creating super admin

--- src/app/utils/seedSuperAdmin.ts
+++ src/app/utils/seedSuperAdmin.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import { envVars } from "../config/env";
+import { IAuthProvider, IUser, Role } from "../modules/user/user.interface";
 import { User } from "../modules/user/user.model";
 import bcryptjs from "bcryptjs";
 
@@ -15,7 +16,24 @@ export const seedSuperAdmin = async () => {
           return;
      }
 
-     const hashedPassword = await bcryptjs.hash(envVars.SUPER_ADMIN_PASSWORD, Number(envVars.BCRYPT_SALT_ROUND))
+     const hashedPassword = await bcryptjs.hash(envVars.SUPER_ADMIN_PASSWORD, Number(envVars.BCRYPT_SALT_ROUND));
+
+     const authProvider : IAuthProvider = {
+          provider: "credentials",
+          providerId: envVars.SUPER_ADMIN_EMAIL
+     }
+
+     const payload: IUser = {
+          name: "Super Admin",
+          role: Role.SUPER_ADMIN,
+          email: envVars.SUPER_ADMIN_EMAIL,
+          password: hashedPassword,
+          isVerified: true,
+          auths: [authProvider]
+     }
+
+     const superAdmin = await User.create(payload);
+     console.log("Super Admin seeded successfully:", superAdmin.email);
 
      } catch(error) {
           console.log("Failed to seed Super Admin:", error);
