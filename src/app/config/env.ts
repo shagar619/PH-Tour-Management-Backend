2f87fedbf2f220f13c0a89bb0d996e9e7589b5f8
shagar619 <ashagar619@gmail.com>
2025-10-18 22:11:17 +0600

    added check auth middlewares

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,13 +6,15 @@ interface EnvConfig {
      PORT: string,
      DB_URL: string,
      NODE_ENV: "development" | "production",
-     BCRYPT_SALT_ROUND: string
+     BCRYPT_SALT_ROUND: string,
+     JWT_ACCESS_EXPIRES: string,
+     JWT_ACCESS_SECRET: string
 }
 
 
 const loadEnvVariables = () : EnvConfig => {
 
-     const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV"];
+     const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET"];
 
      requiredEnvVariables.forEach(key => {
           if(!process.env[key]) {
@@ -26,6 +28,8 @@ const loadEnvVariables = () : EnvConfig => {
      DB_URL: process.env.DB_URL!,
      NODE_ENV: process.env.NODE_ENV as "development" | "production",
      BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+     JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string
      
      }
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,7 +3,8 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { generateToken } from "../../utils/jwt";
+import { envVars } from "../../config/env";
 
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -28,12 +29,10 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
           role: isUserExist.role
      }
 
-     const accessToken = jwt.sign(jwtPayload, "secretKey", {
-          expiresIn: "1d"
-     });
+     const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
 
 
-     return { accessToken, ...jwtPayload };
+     return { accessToken };
 
 };
 
