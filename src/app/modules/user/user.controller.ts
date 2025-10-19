daadc2dfc054b0821b83b11d63610f4200d26f38
shagar619 <ashagar619@gmail.com>
2025-10-19 23:19:41 +0600

    added update user route

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,6 +4,9 @@ import { UserServices } from "./user.service";
 import httpStatus from "http-status-codes";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { verifyToken } from "../../utils/jwt";
+import { JwtPayload } from "jsonwebtoken";
+import { envVars } from "../../config/env";
 
 
 
@@ -51,12 +54,12 @@ const createUser =catchAsync(async (req: Request, res: Response, next: NextFunct
 const updateUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
      const userId = req.params.id;
-     // const token = req.headers.authorization
-    // const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload
+     const token = req.headers.authorization
+     const verifiedToken = verifyToken(token as string, envVars.JWT_ACCESS_SECRET) as JwtPayload
 
-     const verifyToken = req.user;
+     // const verifyToken = req.user;
      const payload = req.body;
-     const user = await UserServices.updateUser(userId, payload, verifyToken);
+     const user = await UserServices.updateUser(userId, payload, verifiedToken);
 
      sendResponse(res, {
           success: true,
@@ -89,7 +92,8 @@ const getAllUsers = catchAsync(async(req: Request, res: Response, next: NextFunc
 
 export const UserControllers = {
      createUser,
-     getAllUsers
+     getAllUsers,
+     updateUser
 }
 
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
 
@@ -12,5 +12,7 @@ const router = Router();
 
 router.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser);
 router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers);
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser);
+// /api/v1/user/:id
 
 export const UserRoutes = router;
