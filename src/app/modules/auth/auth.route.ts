ddb97f3ea0710b9e46d5c42a1e4b4438e6299b3f
shagar619 <ashagar619@gmail.com>
2025-10-20 15:01:01 +0600

    added createNewAccessTokenWithRefreshToken

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,5 +5,6 @@ import { AuthControllers } from "./auth.controller";
 const router = Router();
 
 router.post("/login", AuthControllers.credentialsLogin);
+router.post("/refresh-token");
 
 export const AuthRoutes = router;
