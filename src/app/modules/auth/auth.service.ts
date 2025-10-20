89d77c8d175ac2ac4c136e75db7376cc40a39829
shagar619 <ashagar619@gmail.com>
2025-10-20 12:37:35 +0600

    added refresh token

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -31,8 +31,17 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
 
      const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
 
+     const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES);
 
-     return { accessToken };
+     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+     const { password: pass, ...rest } = isUserExist.toObject();
+
+
+     return { 
+          accessToken,
+          refreshToken,
+          user: rest
+     };
 
 };
 
