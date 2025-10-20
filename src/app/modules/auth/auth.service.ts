2023eaee4db88527ddf145f791b81b4e91bb0e85
shagar619 <ashagar619@gmail.com>
2025-10-20 13:17:40 +0600

    added createUserTokens

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,8 +3,7 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-import { generateToken } from "../../utils/jwt";
-import { envVars } from "../../config/env";
+import { createUserTokens } from "../../utils/userTokens";
 
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
@@ -23,23 +22,29 @@ const credentialsLogin = async (payload: Partial<IUser>) => {
           throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect");
      }
 
-     const jwtPayload = {
-          userId: isUserExist._id,
-          email: isUserExist.email,
-          role: isUserExist.role
-     }
 
-     const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
 
-     const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES);
+
+     // const jwtPayload = {
+     //      userId: isUserExist._id,
+     //      email: isUserExist.email,
+     //      role: isUserExist.role
+     // }
+
+     // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
+
+     // const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES);
+
+
+     const userTokens = createUserTokens(isUserExist);
 
      // eslint-disable-next-line @typescript-eslint/no-unused-vars
      const { password: pass, ...rest } = isUserExist.toObject();
 
 
      return { 
-          accessToken,
-          refreshToken,
+          accessToken: userTokens.accessToken,
+          refreshToken: userTokens.refreshToken,
           user: rest
      };
 
