e519e3b752103e157d5889abfa5d1f1f10f237d3
shagar619 <ashagar619@gmail.com>
2025-10-23 21:19:53 +0600

    handling duplicate error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,12 +3,29 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errorHelpers/AppError";
 import { envVars } from "../config/env";
+import { TErrorSources } from "../interfaces/error.types";
+import { handlerDuplicateError } from "../helpers/handleDuplicateError";
 
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
+     const errorSources: TErrorSources[] = [];
      let statusCode = 500
-     let message = "Something Went Wrong!!"
+     let message = "Something Went Wrong!"
+
+
+
+     // Duplicate Error
+     if(err.code === 11000) {
+          const simplifiedError = handlerDuplicateError(err);
+          statusCode = simplifiedError.statusCode;
+          message = simplifiedError.message;
+     }
+
+
+
+
+
 
      if(err instanceof AppError) {
           statusCode = err.statusCode
