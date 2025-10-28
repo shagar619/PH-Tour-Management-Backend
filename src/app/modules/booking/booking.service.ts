5ac731a9c5f3aa80d51c67a9a2eb242c449dbf17
shagar619 <ashagar619@gmail.com>
2025-10-28 23:50:59 +0600

    implement SSL Commerz for payment system

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -87,8 +87,14 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
           throw error
      }
 
-
 }
+
+
+// SSL Commerz
+
+// Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Complete -> Backend(localhost:5000/api/v1/payment/success) -> Update Payment(PAID) & Booking(CONFIRM) -> redirect to frontend -> Frontend(localhost:5173/payment/success)
+
+// Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Fail / Cancel -> Backend(localhost:5000) -> Update Payment(FAIL / CANCEL) & Booking(FAIL / CANCEL) -> redirect to frontend -> Frontend(localhost:5173/payment/cancel or localhost:5173/payment/fail)
 
 
 
