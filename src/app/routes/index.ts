8011ee2ea337e86deedd4fea60d77244c17d004a
shagar619 <ashagar619@gmail.com>
2025-10-29 12:27:16 +0600

    implement payment all routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { AuthRoutes } from "../modules/auth/auth.route";
 import { DivisionRoutes } from "../modules/division/division.route";
 import { TourRoutes } from "../modules/tour/tour.route";
 import { BookingRoutes } from "../modules/booking/booking.route";
+import { PaymentRoutes } from "../modules/payment/payment.route";
 
 
 export const router = Router();
@@ -32,6 +33,11 @@ const moduleRoutes = [
      {
           path: "/booking",
           route: BookingRoutes
+     },
+
+     {
+          path: "/payment",
+          route: PaymentRoutes
      },
 ]
 
