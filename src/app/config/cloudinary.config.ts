3710c0552f234684d33d26081e34526c2f7c74d1
shagar619 <ashagar619@gmail.com>
2025-10-29 21:04:45 +0600

    added Cloudinary for file uploading

---
+++ src/app/config/cloudinary.config.ts
@@ -0,0 +1,7 @@
+
+
+// Frontend -> Form Data with Image File -> Multer -> Form data -> Req (Body + File)
+
+// Our folder -> image -> form data -> File -> Multer -> Our project /In pc folder(temporary) -> Req.file
+
+//req.file -> cloudinary(req.file) -> url -> mongoose -> mongodb
