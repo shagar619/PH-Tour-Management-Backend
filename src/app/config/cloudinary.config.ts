0ba8e55ac6359b7a5626feeb8981ef2ec135c8c8
shagar619 <ashagar619@gmail.com>
2025-10-29 23:02:45 +0600

    added milter configuration for file upload

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,3 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import { envVars } from "./env";
+
+
+cloudinary.config({
+     cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
+     api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
+     api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET
+})
+
+
+
+
+
+
+export const cloudinaryUpload = cloudinary;
+
+
+
+
 
 
 // Frontend -> Form Data with Image File -> Multer -> Form data -> Req (Body + File)
@@ -5,3 +25,12 @@
 // Our folder -> image -> form data -> File -> Multer -> Our project /In pc folder(temporary) -> Req.file
 
 //req.file -> cloudinary(req.file) -> url -> mongoose -> mongodb
+
+
+
+// const uploadToCloudinary = cloudinary.uploader.upload()
+
+//
+
+//Multer storage cloudinary
+//Our folder -> image -> form data -> File -> Multer -> storage in cloudinary -> url ->  req.file  -> url  -> mongoose -> mongodb
