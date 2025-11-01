fda931d704a9337489096256e7117bcc96004866
shagar619 <ashagar619@gmail.com>
2025-11-01 23:38:27 +0600

    part 7 completed

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -143,18 +143,17 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
           statusCode: httpStatus.ACCEPTED,
           message: "User logged out successfully!",
           data: null
-     })
+     });
 
-})
+});
+
 
 
 const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-     const newPassword = req.body.newPassword;
-     const oldPassword = req.body.oldPassword;
-     const decodedToken = req.user
+     const decodedToken = req.user;
 
-     await AuthService.resetPassword();
+     await AuthService.resetPassword(req.body, decodedToken as JwtPayload);
 
      sendResponse(res, {
           success: true,
