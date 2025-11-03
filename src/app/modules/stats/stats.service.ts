9a4ffac7c3a0c16fe0e75b47b1c0c3e6463f7429
shagar619 <ashagar619@gmail.com>
2025-11-03 21:31:58 +0600

    added Dashboard Analytics API, Payment Stats

--- src/app/modules/stats/stats.service.ts
+++ src/app/modules/stats/stats.service.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Booking } from "../booking/booking.model";
+import { PAYMENT_STATUS } from "../payment/payment.interface";
+import { Payment } from "../payment/payment.model";
 import { Tour } from "../tour/tour.model";
 import { IsActive } from "../user/user.interface";
 import { User } from "../user/user.model";
@@ -314,8 +316,76 @@ const getBookingStats = async () => {
 
 const getPaymentStats = async () => {
 
-     return null;
+     const totalPaymentPromise = Payment.countDocuments();
+
+     const totalPaymentByStatusPromise = Payment.aggregate([
+
+          // stage 1 group
+          {
+               $group: {
+                    _id: "$status",
+                    count: { $sum: 1 }
+               }
+          }
+     ]);
+
+     const totalRevenuePromise = Payment.aggregate([
+
+          // stage 1 - match
+          {
+               $match: { status: PAYMENT_STATUS.PAID }
+          },
+
+          {
+               $group: {
+                    _id: null,
+                    totalRevenue: { $sum: "$amount" }
+               }
+          }
+     ]);
+
+     const avgPaymentAmountPromise = Payment.aggregate([
+
+          // stage 1 group stage
+          {
+               $group: {
+                    _id: null,
+                    avgPaymentAMount: { $avg: "$amount" }
+               }
+          }
+     ]);
+
+     const paymentGatewayDataPromise = Payment.aggregate([
+
+          // stage 1 - group
+          {
+               $group: {
+                    _id: { $ifNull: ["$paymentGatewayData.status", "UNKNOWN"] },
+                    count: { $sum: 1 }
+               }
+          }
+     ]);
+
+
+     const [totalPayment, totalPaymentByStatus, totalRevenue, avgPaymentAmount, paymentGatewayData] = await Promise.all([
+          totalPaymentPromise,
+          totalPaymentByStatusPromise,
+          totalRevenuePromise,
+          avgPaymentAmountPromise,
+          paymentGatewayDataPromise
+     ]);
+
+
+     return {
+          totalPayment,
+          totalPaymentByStatus,
+          totalRevenue,
+          avgPaymentAmount,
+          paymentGatewayData
+     }
 }
+
+
 
 
 export const StatsService = {
@@ -324,6 +394,11 @@ export const StatsService = {
      getTourStats,
      getUserStats
 }
+
+
+
+
+
 
 
 
